ccef0b1dbcb0820e6c96325e35811d2bf52fd210
HuvaaKoodia <huvaakoodia@gmail.com>
2014-01-15 16:21:20 +0200

    Map system foundation

---
+++ Assets/_Game/Map/TileMain.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileMain : MonoBehaviour 
+{
+	public Rect size;
+	public Vector3 position;
+	public GameObject tileObject;
+	
+	public TileData Data{get;private set;}
+	public void SetData(TileData data){
+		Data=data;
+	}
+
+	public bool Blocked ()
+	{
+		return tileObject!=null;
+	}
+
+	
+}
