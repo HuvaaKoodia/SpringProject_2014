964ea1d5b14d0c703307a176333a918ccfd11a1d
Pikkuninja <jsaarinen92@gmail.com>
2014-01-15 16:31:43 +0200

    Data classes and XML reading

---
+++ Assets/_Game/Enemies/EnemyData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyData
+{
+    public string Type { get; private set; }
+
+    public int Health { get; private set; }
+    public int Damage { get; private set; }
+
+    public EnemyData(string type, int health, int damage)
+    {
+        this.Type = type;
+
+        this.Health = health;
+        this.Damage = damage;
+    }
+}
---
+++ Assets/_Game/Obstacles/ObstacleData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleData
+{
+    public string Type { get; private set; }
+    
+    public int Health { get; private set; }
+
+    public ObstacleData(string type, int health)
+    {
+        this.Type = type;
+
+        this.Health = health;
+    }
+}
