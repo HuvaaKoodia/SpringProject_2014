1554ce62e92161a88e5abda463c843e637ba7c86
SalSatSat <salsatsat@gmail.com>
2014-05-08 16:46:33 +0300

    Systems and prefabs for effects 1

---
+++ Assets/Game/Scripts/System/EmitSystem.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmitSystem : MonoBehaviour {
+	
+	public GameObject emit;
+	public float delay;
+	public float limit;
+
+	private bool emit_active;
+	private float ticks = 0.0f;
+	private float ticks1 = 0.0f;
+
+	void Awake()
+	{
+		emit_active = false;
+		emit.particleSystem.Pause();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//temporary
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			ActivateSmoke();
+		}
+
+		if(emit_active)
+		{
+			UpdateTimer();
+			EmitSmoke();
+		}
+	}
+
+	public void ActivateSmoke()
+	{
+		emit_active = true;
+		emit.particleSystem.Play();
+	}
+
+	private void EmitSmoke()
+	{
+		if(ticks >= delay)
+		{
+			emit.particleSystem.Play();
+			ticks = 0.0f;
+		}
+		else
+		{
+			ticks += Time.deltaTime;
+		}
+	}
+
+	private void UpdateTimer()
+	{
+		if(ticks1 >= limit)
+		{
+			emit_active = false;
+			emit.particleSystem.Stop();
+			ticks1 = 0.0f;
+		}
+		else
+		{
+			ticks1 += Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Game/Scripts/System/MechWarningSystem.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MechWarningSystem : MonoBehaviour {
+	
+	public List<Light> warning_lights;																			//instantiate a list for the warning lights in the various TilePrefabs under TestObjects of GameScene																		
+	public float delay;																							//instantiate time to delay by in inspector
+	
+	private float ticks;																						//instantiate time since last toggle
+	private bool light_flicker;																					//instantiate boolean to allow light to flick
+	private bool power_on=true;																					//instantiate boolean to allow electricity to flow
+
+	public AudioClip warning_sound;
+
+	// Use this for initialization
+	void Start () {
+		light_flicker = false;
+		ticks = 0.0f;
+		
+		EnableLights();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(power_on)
+		{
+			Flicker ();
+			PlaySound();
+		}
+	}
+
+	public bool PowerOn {
+		get {return power_on;}
+		set{
+			power_on=value;
+			EnableLights();
+		}
+	}
+
+	public void EnableLights()
+	{
+		for(int i = 0; i < warning_lights.Count; i++)
+		{
+			var warning_light=warning_lights[i];
+			bool light_on=true;
+			
+			if(warning_light != null)
+			{
+				if (power_on){
+					light_on = light_flicker;
+				}
+				else{
+					light_on = false;
+				}
+				
+				warning_light.enabled=light_on;
+			}
+		}
+	}
+	
+	//function to set white light to flicker
+	private void Flicker()
+	{
+		//toggling of enabled value of the white lights in TilePrefabs under TestObjects in GameScene
+		if(ticks >= delay)
+		{
+			light_flicker = !light_flicker;
+			
+			ticks = 0.0f;
+			EnableLights();
+			
+			//set new random delay
+			if (light_flicker){
+				delay=Subs.GetRandom(10,100)*0.001f;
+			}
+			else{
+				if (Subs.RandomPercent()<20){
+					delay=Subs.GetRandom(10,100)*0.001f;
+				}
+				else
+					delay=Subs.GetRandom(1000,4000)*0.001f;
+			}
+			
+		}
+		//increment ticks every second
+		else
+		{
+			ticks += Time.deltaTime;
+		}
+	}
+
+	private void PlaySound()
+	{
+		AudioSource.PlayClipAtPoint(warning_sound, transform.position);
+	}
+}
