45183fdc896942d4d9de8dbf237408efc9b0aa8a
Pikkuninja <jsaarinen92@gmail.com>
2014-05-27 17:00:46 +0300

    Data console and Power generator sounds

---
+++ Assets/Game/Scripts/Objects/WorldObjects/WorldObjectSound.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldObjectSound : MonoBehaviour {
+
+	public bool ConstantLoop;
+
+	public float WaitTimeBetweenLoops;
+
+	float currentWaitTime;
+
+	// Use this for initialization
+	void Awake() {
+		currentWaitTime = Subs.RandomFloat() * WaitTimeBetweenLoops;
+
+		if (ConstantLoop)
+		{
+			audio.loop = true;
+			audio.Play();
+		}
+		else
+			audio.loop = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ConstantLoop) return;
+
+		currentWaitTime += Time.deltaTime;
+
+		if (currentWaitTime > WaitTimeBetweenLoops)
+		{
+			audio.Play();
+			currentWaitTime = 0.0f;
+		}
+	}
+}
